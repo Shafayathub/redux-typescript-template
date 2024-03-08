3ab49e08e5fe471f57144e2a901eb8a76954af6b
Shafayathub <shafayat.ms@gamilcom>
2024-03-08 16:29:21 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="mt-10 text-5xl text-center">
+          Let's start Apollo journey
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
